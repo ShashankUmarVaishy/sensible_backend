2c5e4041087d46336b4e7915f29508f7f58c8997
Shashank Umar Vaishy <umarshashank@gmail.com>
2025-06-15 19:10:47 +0530

    updated controller of notification

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -25,6 +25,8 @@ if (process.env.FIREBASE_CREDENTIAL_JSON) {
     const decoded = Buffer.from(process.env.FIREBASE_CREDENTIAL_BASE64, 'base64').toString();
     
     serviceAccount = JSON.parse(decoded);
+    console.log(serviceAccount.private_key);
+    console.log("Firebase admin initialized successfully : ", serviceAccount);
     
   } catch (err) {
     console.error("Failed to parse FIREBASE_CREDENTIAL_JSON:", err);
